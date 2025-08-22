9404513edbd1d37357b4a1ab2d1ce6e8e2824178
Masaki Ishizaka <masaki.ishizaka@giftee.co>
2025-08-23 00:11:55 +0900

    feat(view): 横幅が画面幅にピッタリくっつくようにする

--- src/components/ide/Window.tsx
+++ src/components/ide/Window.tsx
@@ -16,6 +16,7 @@ export const IDEWindow = () => {
         width: "100%",
         height: "100%",
         backgroundColor: Colors.ide.background,
+        overflow: "hidden",
       }}
     >
       <Header title="test" />
