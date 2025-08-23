009e60a5c9ec1963d93b86d0749b94c6fabebd9f
OdenTakashi <odentakashidesu2@gmail.com>
2025-08-23 21:05:05 +0900

    fix typo

--- src/scenes/terminal/terminal_20.tsx
+++ src/scenes/terminal/terminal_20.tsx
@@ -2,6 +2,6 @@ import type { TerminalValue } from "../../types/TerminalValue";
 
 export default [
   {
-    value: "Object.send(:remove_const, :ActionManager)",
+    value: "Object.send(:remove_const, :Action)",
   },
 ] as TerminalValue[];
