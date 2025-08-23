2de048e1966317a20b129510074a9830fff940a0
OdenTakashi <odentakashidesu2@gmail.com>
2025-08-23 17:00:20 +0900

    yarn formatの指摘箇所を修正

--- src/scenes/ai/ai_01.tsx
+++ src/scenes/ai/ai_01.tsx
@@ -3,8 +3,7 @@ import type { ChatType } from "../../types/Chat";
 export default [
   {
     id: "1",
-    content:
-      "chatで感情は不要なので、削除します",
+    content: "chatで感情は不要なので、削除します",
     isUser: false,
     timestamp: new Date("2024-03-20T10:02:30"),
   },
