b42fd8658848573bc38bc99bb1cdbc8eaac51d15
Masaki Ishizaka <masaki.ishizaka@giftee.co>
2025-08-23 11:36:16 +0900

    feat(view): シーンがタブで変化するようにする

--- src/components/ide/Editor.tsx
+++ src/components/ide/Editor.tsx
@@ -1,7 +1,23 @@
-import type { LineType } from "../../types/LineValue";
 import { sceneReadme } from "../../scenes/readme";
+import { scene01 } from "../../scenes/01";
+import { scene02 } from "../../scenes/02";
+import { useState } from "react";
+import type { SceneType } from "../../types/Scene";
 
-export const Editor = ({ lines = sceneReadme.lines }: { lines?: LineType[] }) => {
+const scenes: SceneType[] = [sceneReadme, scene01, scene02];
+
+export const Editor = () => {
+  const [sceneIndex, setSceneIndex] = useState(0);
+
+  // Tabキーが押されたときの処理
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Tab") {
+      event.preventDefault(); // デフォルトのTab移動を防ぐ
+
+      setSceneIndex(prevIndex => (prevIndex < scenes.length - 1 ? prevIndex + 1 : 0));
+    }
+  };
+
   return (
     <div
       style={{
@@ -11,9 +27,11 @@ export const Editor = ({ lines = sceneReadme.lines }: { lines?: LineType[] }) =>
         display: "flex",
         flexDirection: "column",
       }}
+      tabIndex={0} // キーボードイベントを受け取れるようにする
+      onKeyDown={handleKeyDown}
     >
-      {lines.map((line, index) => (
-        <div style={{ display: "flex", flexDirection: "row" }}>
+      {scenes[sceneIndex].lines.map((line, index) => (
+        <div key={index} style={{ display: "flex", flexDirection: "row", gap: 4 }}>
           <Number rowNumber={index + 1} />
           <div style={{ display: "flex", flexDirection: "row", gap: 4 }}>{line.value}</div>
         </div>
---
+++ src/scenes/02.tsx
@@ -0,0 +1,86 @@
+import { Tab } from "../components/ide/Tab";
+import { Token } from "../components/ide/Token";
+import type { SceneType } from "../types/Scene";
+
+export const scene02: SceneType = {
+  lines: [
+    {
+      value: (
+        <>
+          <Token value="class" />
+          <Token value="User" />
+        </>
+      ),
+    },
+    {
+      value: (
+        <>
+          <Tab />
+          <Token value="name" />
+          <Token value=":" />
+          <Token value="string" />
+        </>
+      ),
+    },
+    {
+      value: (
+        <>
+          <Tab />
+          <Token value="age" />
+          <Token value=":" />
+          <Token value="number" />
+        </>
+      ),
+    },
+    {
+      value: (
+        <>
+          <Tab />
+          <Token value="address" />
+          <Token value=":" />
+          <Token value="string" />
+        </>
+      ),
+    },
+    {
+      value: <></>,
+    },
+    {
+      value: (
+        <>
+          <Tab />
+          <Token value="def" />
+          <Token value="greet" />
+          <Token value="(" />
+          <Token value="name" />
+          <Token value=")" />
+        </>
+      ),
+    },
+    {
+      value: (
+        <>
+          <Tab />
+          <Tab />
+          <Token value="puts" />
+          <Token value='"Hello, #{name}!' />
+        </>
+      ),
+    },
+    {
+      value: (
+        <>
+          <Tab />
+          <Token value="end" />
+        </>
+      ),
+    },
+    {
+      value: (
+        <>
+          <Token value="end" />
+        </>
+      ),
+    },
+  ],
+};
