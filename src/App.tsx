0f76056fd9190ee264592ff17b1d2ca3eee6b091
OdenTakashi <odentakashidesu2@gmail.com>
2025-08-23 22:32:58 +0900

    add @vercel/analytics/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { IDEWindow } from "./components/ide/Window";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <>
+      <Analytics />
       <IDEWindow />
     </>
   );
