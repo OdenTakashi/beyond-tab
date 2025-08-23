c84ec5767d92d3a1259b966d5c111b0e7ab4fa45
OdenTakashi <odentakashidesu2@gmail.com>
2025-08-23 16:58:38 +0900

    aiチャットのシーンを追加

---
+++ src/scenes/ai/ai_02.tsx
@@ -0,0 +1,11 @@
+import type { ChatType } from "../../types/Chat";
+
+export default [
+  {
+    id: "2",
+    content:
+      "chatで行動権利は不要なので、削除します",
+    isUser: false,
+    timestamp: new Date("2024-03-20T10:02:30"),
+  },
+] as ChatType[];
