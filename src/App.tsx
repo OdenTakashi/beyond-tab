dcc1efb7c0fc50fbe47b018e8189f0180a8881d3
OdenTakashi <odentakashidesu2@gmail.com>
2025-08-23 22:34:41 +0900

    yarn format

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { IDEWindow } from "./components/ide/Window";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
