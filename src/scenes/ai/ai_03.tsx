543347054beab5e1541227e4a4d000c68b5d8721
Masaki Ishizaka <masaki.ishizaka@giftee.co>
2025-08-23 20:48:43 +0900

    feat: chatでという問題を削除

--- src/scenes/ai/ai_03.tsx
+++ src/scenes/ai/ai_03.tsx
@@ -3,7 +3,7 @@ import type { ChatType } from "../../types/Chat";
 export default [
   {
     id: "3",
-    content: "chatで人格は不要なので、削除します",
+    content: "人格は不要なので、削除します",
     isUser: false,
     timestamp: new Date("2024-03-20T10:02:30"),
   },
